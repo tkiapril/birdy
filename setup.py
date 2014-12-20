9f3235bcd2899a75c22f5f3a92cdbe721c209062
Tki April <tki@tki.so>
2014-12-20 16:19:19 +0900

    Fixes according to flake8 (conform PEP 8)

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
-#!/usr/bin/env python
 from setuptools import setup
 from birdy import __author__, __version__
-import os
-import sys
 
 setup(
-    name = 'birdy',
-    version = __version__,
-    install_requires = (
+    name='birdy',
+    version=__version__,
+    install_requires=(
         'requests>=1.2.3',
         'requests_oauthlib>=0.3.2',
     ),
-    author = 'Mitja Pagon',
-    author_email = 'mitja@inueni.com',
-    license = open('LICENSE').read(),
-    url = 'https://github.com/inueni/birdy/',
-    keywords = 'twitter api tweet birdy search',
-    description = 'birdy is a super awesome Twitter API client for Python.',
-    long_description = open('README.rst').read(),
-    include_package_data = True,
-    packages = (
+    author=__author__[:__author__.find('<')-1],
+    author_email=__author__[__author__.find('<')+1:__author__.find('>')],
+    license=open('LICENSE').read(),
+    url='https://github.com/inueni/birdy/',
+    keywords='twitter api tweet birdy search',
+    description='birdy is a super awesome Twitter API client for Python.',
+    long_description=open('README.rst').read(),
+    include_package_data=True,
+    packages=(
         'birdy',
     ),
     classifiers=(
